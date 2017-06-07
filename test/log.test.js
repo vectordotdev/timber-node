55fd895d41ab50289396107e9e74b5d830745a72
Garet McKinley <self@mediachicken.com>
2017-06-07 13:30:04 -0400

    replace transformer test with log test

---
+++ test/log.test.js
@@ -0,0 +1,7 @@
+import Log from '../src/utils/log';
+
+describe('Log Transformer', () => {
+  it('exports a function', () => {
+    expect(typeof Log).toBe('function');
+  });
+});
--- test/transformer.test.js
+++
@@ -1,7 +0,0 @@
-import transform from '../src/utils/transform';
-
-describe('transform', () => {
-  it('exports a function', () => {
-    expect(typeof transform).toBe('function');
-  });
-});
