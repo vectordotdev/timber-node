0ecc960008be1539dd514b31597ef55d37c31bc4
Garet McKinley <self@mediachicken.com>
2017-06-20 13:46:25 -0400

    allow attaching multiple transports to a single stream

--- src/utils/attach.js
+++ src/utils/attach.js
@@ -1,41 +1,36 @@
-import { Writable } from 'stream'
 import Log from '../log'
 import debug from './debug'
 
 /**
  * Attaches a transport stream to a writeable stream.
  *
- * @param {Writable} stream - the transport stream
+ * @param {Array} transports - array of transports to attach to the stream
  * @param {Writable} toStream - the stream your transport will attach to
  * @param {Object} options - configuration options
  * @param {boolean} options.applyBackPressure
  */
-const attach = (stream, toStream, { applyBackPressure = false } = {}) => {
-  // Ensure the stream is Writable
-  if (!(stream instanceof Writable)) {
-    throw new Error('stream must be of type Writable')
-  }
-
+const attach = (transports, toStream, { applyBackPressure = false } = {}) => {
   // Store refs to standard logging utilities
   const originalWrite = toStream.write
 
-  toStream.write = (write => {
-    return function(message, encoding, fd) {
-      const log = message instanceof Log ? message : new Log(message)
-      // transform the message string into a schema adhering object
-      const written = stream.write(log.data, encoding, fd)
+  debug(`attaching ${transports.length} transports to stream`)
 
-      debug(`Logged: ${JSON.stringify(log.data, null, 2)}`)
+  toStream.write = (message, encoding, fd) => {
+    const log = message instanceof Log ? message : new Log(message)
 
-      // If we want to allow back pressure, listen for
-      // the drain event and try once the buffer is cleared
+    for (let i = 0; i < transports.length; i++) {
+      const transport = transports[i]
+      const written = transport.write(
+        transport.acceptsObject ? log.data : log.data.message,
+        encoding,
+        fd
+      )
+
       if (!written && applyBackPressure) {
-        stream.once('drain', () => stream.write(...arguments))
+        transport.once('drain', () => transport.write(...arguments))
       }
-
-      return written
     }
-  })(process.stdout.write)
+  }
 
   return {
     detach: () => {
