13702db76e4d0d096e27da583c8fbd6a1f987e42
Garet McKinley <self@mediachicken.com>
2017-08-07 13:58:24 -0400

    add custom event support to winston transport

---
+++ src/events/index.js
@@ -0,0 +1,9 @@
+import Custom from './custom'
+import http_server_request from './http_server_request'
+import http_server_response from './http_server_response'
+
+export default {
+  Custom,
+  http_server_request,
+  http_server_response,
+}
