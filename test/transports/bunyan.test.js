f5e09bc6fcbbc9d31c4095bf1dbf950df38e851f
Garet McKinley <self@mediachicken.com>
2017-08-31 22:43:45 -0400

    add tests for bunyan transport

---
+++ test/transports/bunyan.test.js
@@ -0,0 +1,103 @@
+import { Writable, Readable } from 'stream';
+import bunyan from 'bunyan';
+import BunyanTransport from '../../src/transports/bunyan';
+
+class TestWriteStream extends Writable {
+  constructor() { super({ objectMode: true }) }
+  _write(){}
+}
+class TestReadStream extends Readable {}
+
+describe('Bunyan Transport', () => {
+  it('should throw an error when no stream is provided', () => {
+    expect(() => { new BunyanTransport() }).toThrow();
+  })
+
+  it('can be added to winston', () => {
+    const stream = new TestWriteStream();
+
+    expect(() => {
+      const log = bunyan.createLogger({
+        name: 'Timber Logger',
+        stream: new BunyanTransport({ stream })
+      });
+    }).not.toThrow();
+  })
+
+  it('logs messages to stream', () => {
+    const message = 'Test log message';
+    const level = 'info';
+    const stream = new TestWriteStream();
+    stream.cork();
+
+    const log = bunyan.createLogger({
+      name: 'Timber Logger',
+      stream: new BunyanTransport({ stream })
+    });
+
+    log[level](message);
+
+    const written = stream._writableState.getBuffer().pop().chunk;
+
+    expect(written.message).toBe(message);
+    expect(written.level).toBe(level);
+  })
+
+  it('augments logs with custom context', () => {
+    const message = 'Test log message';
+    const level = 'info';
+    const context = { foo: 'bar' };
+    const stream = new TestWriteStream();
+    stream.cork();
+
+    const log = bunyan.createLogger({
+      name: 'Timber Logger',
+      stream: new BunyanTransport({ stream })
+    });
+
+    log[level]({ context }, message);
+
+    const written = stream._writableState.getBuffer().pop().chunk;
+
+    expect(written.context).toMatchObject(context);
+  })
+
+  it('augments logs with custom events', () => {
+    const message = 'Test log message';
+    const level = 'info';
+    const event = { test_event_name: { foo: 'bar' } };
+    const stream = new TestWriteStream();
+    stream.cork();
+
+    const log = bunyan.createLogger({
+      name: 'Timber Logger',
+      stream: new BunyanTransport({ stream })
+    });
+
+    log[level]({ event }, message);
+
+    const written = stream._writableState.getBuffer().pop().chunk;
+
+    expect(written.event).toMatchObject({ custom: event });
+  })
+
+  it('augments logs with metadata', () => {
+    const message = 'Test log message';
+    const level = 'info';
+    const meta = { foo: 'bar' };
+    const stream = new TestWriteStream();
+    stream.cork();
+
+    const log = bunyan.createLogger({
+      name: 'Timber Logger',
+      stream: new BunyanTransport({ stream })
+    });
+
+    log[level](meta, message);
+
+    const written = stream._writableState.getBuffer().pop().chunk;
+
+    expect(written.meta).toMatchObject(meta);
+  })
+})
+
